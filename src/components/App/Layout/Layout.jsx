9ba45cd336d5c11c0de09519ca136964f85d44ed
TordisLaksa <88707360+TordisLaksa@users.noreply.github.com>
2022-09-05 07:54:53 +0100

    Initialize project using Create React App

---
+++ src/components/App/Layout/Layout.jsx
@@ -0,0 +1,24 @@
+import { useEffect } from "react";
+
+
+//Det her er til hver eneste side for at skabe en gennemgående dynamisk model til hver side ift seo og indhold
+const Layout = props => {
+    useEffect(() => {
+        document.title = props.title
+        if(props.description){
+            document.querySelector('meta[name="description"]')
+            .setAttribute('content', props.description)
+        }
+    },[props.title, props.description])
+    
+    return(
+        <>
+            <h1>{props.title}</h1>
+            {props.children}
+        </>
+    )
+}
+
+
+
+ export { Layout }
